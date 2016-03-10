8a53e0c0dc29a91490e4e9cce828577517f4bde0
Byron Hulcher <byronhulcher@gmail.com>
2016-03-10 15:39:25 -0500

    Updated spinner test to support props

--- tests/components/spinner-test.js
+++ tests/components/spinner-test.js
@@ -3,11 +3,17 @@ jest.dontMock('../../js/components/spinner');
 var React = require('react/addons');
 var TestUtils = React.addons.TestUtils;
 var Spinner = require('../../js/components/spinner');
+var skinConfig = require('../../config/skin.json');
+var Utils = require('../../js/components/utils');
 
 describe('Spinner', function () {
   it('tests spinner', function () {
-
+    
+    var clonedSkinConfig = Utils.clone(skinConfig);
+    var mockProps = {
+      skinConfig: clonedSkinConfig
+    }
     //Render spinner into DOM
-    var DOM = TestUtils.renderIntoDocument(<Spinner />);
+    var DOM = TestUtils.renderIntoDocument(<Spinner {... mockProps} />);
   });
 });
